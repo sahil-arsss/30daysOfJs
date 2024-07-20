84c20850d03447f2d8b0f3be3a5117301ed88407
sahil-arsss <sahilchaudhary8876@gmail.com>
2024-07-20 11:49:11 +0530

    day2

---
+++ day2.js
@@ -0,0 +1,101 @@
+//activity 1
+let a=24;
+let b=12;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);
+
+
+
+//activity 2
+let c=25;
+c+=1;
+console.log(c);
+
+c=27;
+c-=1;
+console.log(c);
+
+
+
+
+
+//activity 3
+a=10;
+b=11;
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+
+let x=11;
+let y="11";
+console.log(x==y);   
+console.log(x===y);  /// also compare data type
+
+
+
+
+
+//activity 4
+x=1
+y=2
+a=1
+b=3
+if(x==a && y==b)
+{
+    console.log("both condition are true");
+}
+if(x==a || y==b)
+{
+    console.log("one of them  or may be both  true");
+}
+if(!(x==a && y==b))
+{
+        console.log(" condition is true ");
+}
+
+
+
+
+//activity 5
+x=-10;
+x<0?console.log("negative"):console.log("positive");
+
+
+
+
+
+
+
+//feature
+//1.
+ a=24;
+ b=12;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);
+
+
+//2.
+a=10;
+b=11;
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+
+ x=11;
+ y="11";
+console.log(x==y);   
+console.log(x===y);  /// also compare data type
+
+
+
+//3.
+x=-10;
+x<0?console.log("negative"):console.log("positive");
+x=10;
+x<0?console.log("negative"):console.log("positive");
